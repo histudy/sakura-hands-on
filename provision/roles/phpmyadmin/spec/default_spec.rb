4908535fbeeba863eb2b3558fac5544a4139b406
wate <pc.poisoning@gmail.com>
2017-03-05 17:07:03 +0900

    パッケージからインストールするように変更

--- provision/roles/phpmyadmin/spec/default_spec.rb
+++ provision/roles/phpmyadmin/spec/default_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
-describe file(property['phpmyadmin_dest']) do
+describe package('phpMyAdmin') do
+  it { should be_installed }
+end
+
+describe file(property['phpmyadmin_upload_dir']) do
   it { should exist }
   it { should be_directory }
 end
-describe file(property['phpmyadmin_dest'] + '/config.inc.php') do
+describe file(property['phpmyadmin_save_dir']) do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file('/etc/httpd/conf.d/phpMyAdmin.conf') do
+  it { should_not exist }
+end
+
+describe file('/etc/phpMyAdmin/config.inc.php') do
   it { should exist }
   it { should be_file }
 end
