097a5ae56c59e46233a982bbc163bb265e3e3469
wate <pc.poisoning@gmail.com>
2017-03-06 00:13:32 +0900

    dehydrated用フックスクリプトのインストール指定処理を追記

--- provision/roles/lexicon/spec/default_spec.rb
+++ provision/roles/lexicon/spec/default_spec.rb
@@ -5,8 +5,16 @@ property['lexicon_dependency_packages'].each do |pkg|
     it { should be_installed }
   end
 end
+
 property['lexicon_dependency_pip_packages'].each do |pkg|
   describe package(pkg) do
     it { should be_installed.by('pip') }
+  end
+end
+
+if property['lexicon_install_dehydrated_hook']
+  describe file(property['lexicon_hook_dest']) do
+    it { should exist }
+    it { should be_file }
   end
 end
